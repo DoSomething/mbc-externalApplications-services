e7a550dcea3947378e0ca3419efe03f396260870
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-06-06 16:50:44 -0400

    Move app config to include file

--- mbc-externalApplications-events.php
+++ mbc-externalApplications-events.php
@@ -9,60 +9,20 @@
  *   - User votes result in tagging user accounts in Mailchimp with vote
  */
 
-use DoSomething\MBStatTracker\StatHat;
+date_default_timezone_set('America/New_York');
 
 // Load up the Composer autoload magic
 require_once __DIR__ . '/vendor/autoload.php';
+use DoSomething\MBC_ExternalApplications\MBC_ExternalApplications_Events;
 
-// Load configuration settings common to the Message Broker system
-// symlinks in the project directory point to the actual location of the files
-require_once __DIR__ . '/mb-secure-config.inc';
-
-require_once __DIR__ . '/MBC_ExternalApplications_Events.class.inc';
-require_once __DIR__ . '/messagebroker-config/MB_Configuration.class.inc';
+// Load configuration settings specific to this application
+require_once __DIR__ . '/mbc-externalApplications-events.config.inc';
 
 
-// Settings
-$credentials = array(
-  'host' =>  getenv("RABBITMQ_HOST"),
-  'port' => getenv("RABBITMQ_PORT"),
-  'username' => getenv("RABBITMQ_USERNAME"),
-  'password' => getenv("RABBITMQ_PASSWORD"),
-  'vhost' => getenv("RABBITMQ_VHOST"),
-);
-$settings = array(
-  'stathat_ez_key' => getenv("STATHAT_EZKEY"),
-  'ds_drupal_api_host' => getenv('DS_DRUPAL_API_HOST'),
-  'ds_drupal_api_port' => getenv('DS_DRUPAL_API_PORT'),
-);
-
-$config = array();
-$source = __DIR__ . '/messagebroker-config/mb_config.json';
-$mb_config = new MB_Configuration($source, $settings);
-$externalApplicationsExchange = $mb_config->exchangeSettings('directExternalApplicationsExchange');
-
-$config['exchange'] = array(
-  'name' => $externalApplicationsExchange->name,
-  'type' => $externalApplicationsExchange->type,
-  'passive' => $externalApplicationsExchange->passive,
-  'durable' => $externalApplicationsExchange->durable,
-  'auto_delete' => $externalApplicationsExchange->auto_delete,
-);
-$config['queue'][] = array(
-  'name' => $externalApplicationsExchange->queues->externalApplicationEventQueue->name,
-  'passive' => $externalApplicationsExchange->queues->externalApplicationEventQueue->passive,
-  'durable' => $externalApplicationsExchange->queues->externalApplicationEventQueue->durable,
-  'exclusive' => $externalApplicationsExchange->queues->externalApplicationEventQueue->exclusive,
-  'auto_delete' => $externalApplicationsExchange->queues->externalApplicationEventQueue->auto_delete,
-  'bindingKey' => $externalApplicationsExchange->queues->externalApplicationEventQueue->binding_key,
-);
-
-
-
-echo '------- mbc-externalApplications-events START: ' . date('D M j G:i:s T Y') . ' -------', PHP_EOL;
+echo '------- mbc-externalApplications-events START: ' . date('j D M Y G:i:s T') . ' -------', PHP_EOL;
 
 // Kick off
 $mb = new MessageBroker($credentials, $config);
-$mb->consumeMessage(array(new MBC_externalApplications_events($credentials, $settings), 'consumeQueue'));
+$mb->consumeMessage(array(new MBC_ExternalApplications_Events($credentials, $settings), 'consumeQueue'));
 
-echo '------- mbc-externalApplications-events END: ' . date('D M j G:i:s T Y') . ' -------', PHP_EOL;
+echo '------- mbc-externalApplications-events END: ' . date('j D M Y G:i:s T') . ' -------', PHP_EOL;
