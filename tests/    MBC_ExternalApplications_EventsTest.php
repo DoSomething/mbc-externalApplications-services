f150e839bd3e3c24ce3c57ef4c068591f0d1edf7
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-06-08 17:44:04 -0400

    Stub tests

---
+++ tests/    MBC_ExternalApplications_EventsTest.php
@@ -0,0 +1,25 @@
+<?php
+ 
+use DoSomething\MBC_ExternalApplications\MBC_ExternalApplications_Events;
+
+  // Including that file will also return the autoloader instance, so you can store
+  // the return value of the include call in a variable and add more namespaces.
+  // This can be useful for autoloading classes in a test suite, for example.
+  // https://getcomposer.org/doc/01-basic-usage.md
+  $loader = require_once __DIR__ . '/../vendor/autoload.php';
+ 
+class MBC_ExternalApplications_EventsTest extends PHPUnit_Framework_TestCase {
+  
+  public function setUp(){ }
+  public function tearDown(){ }
+ 
+  public function testFindNextTargetFile()
+  {
+
+      $this->assertTrue(TRUE);
+      
+    }
+
+  }
+ 
+}
