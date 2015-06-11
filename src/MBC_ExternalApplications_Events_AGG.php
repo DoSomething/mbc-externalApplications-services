7459df82f147f679893f8340582f3e61eb5537e5
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-06-11 18:52:24 -0400

    Move to MB_Configuration->constructConfig()

--- src/MBC_ExternalApplications_Events_AGG.php
+++ src/MBC_ExternalApplications_Events_AGG.php
@@ -72,34 +72,18 @@ class MBC_ExternalApplications_Events_AGG
 
     $payload = array(
       'mobile' => $message['mobile'],
+      'first_name' => '',
+      'birthdate_timestamp' => $message['candidate_name'],
       'candidate_name' => $message['candidate_name'],
+      'candidate_gender' => $message['candidate_gender'],
       'activity' => $message['activity'],
       'mc_opt_in_path_id' => $message['mc_opt_in_path_id']
     );
-
     $payload = serialize($payload);
 
-    $config = array();
-    $configSource = __DIR__ . '/../messagebroker-config/mb_config.json';
-    $mb_config = new MB_Configuration($configSource, $this->settings);
-    $transactionalExchange = $mb_config->exchangeSettings('transactionalExchange');
-
-    $config['exchange'] = array(
-      'name' => $transactionalExchange->name,
-      'type' => $transactionalExchange->type,
-      'passive' => $transactionalExchange->passive,
-      'durable' => $transactionalExchange->durable,
-      'auto_delete' => $transactionalExchange->auto_delete,
-    );
-    $config['queue'][] = array(
-      'name' => $transactionalExchange->queues->mobileCommonsQueue->name,
-      'passive' => $transactionalExchange->queues->mobileCommonsQueue->passive,
-      'durable' => $transactionalExchange->queues->mobileCommonsQueue->durable,
-      'exclusive' => $transactionalExchange->queues->mobileCommonsQueue->exclusive,
-      'auto_delete' => $transactionalExchange->queues->mobileCommonsQueue->auto_delete,
-      'binding_pattern' => $transactionalExchange->queues->mobileCommonsQueue->binding_pattern,
-    );
-    $config['routing_key'] = 'user.registration.agg';
+    $mbConfig = new MB_Configuration($this->settings, CONFIG_PATH . '/mb_config.json');
+    $config = $mbConfig->constructConfig('transactionalExchange', array('mobileCommonsQueue'));
+    $config['routingKey'] = 'user.registration.agg';
 
     $mbMobileCommons = new \MessageBroker($this->credentials, $config);
     $mbMobileCommons->publishMessage($payload);
@@ -135,7 +119,7 @@ class MBC_ExternalApplications_Events_AGG
 
     $mbConfig = new MB_Configuration($this->settings, CONFIG_PATH . '/mb_config.json');
     $config = $mbConfig->constructConfig('transactionalExchange', array('transactionalQueue'));
-    $config['routing_key'] = 'vote.cgg.transactional';
+    $config['routingKey'] = 'vote.agg.transactional';
 
     $payload = serialize($message);
 
