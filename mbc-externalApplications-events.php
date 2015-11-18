29ac5a3a4549ca1f3845ec223206dd37770cb3b4
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-11-18 18:10:56 -0500

    Modernize basic app startup

--- mbc-externalApplications-events.php
+++ mbc-externalApplications-events.php
@@ -11,19 +11,21 @@
 
 date_default_timezone_set('America/New_York');
 define('CONFIG_PATH',  __DIR__ . '/messagebroker-config');
+// The number of messages for the consumer to reserve with each callback
+// See consumeMwessage for further details.
+// Necessary for parallel processing when more than one consumer is running on the same queue.
+define('QOS_SIZE', 1);
 
 // Load up the Composer autoload magic
 require_once __DIR__ . '/vendor/autoload.php';
-use DoSomething\MBC_ExternalApplications\MBC_ExternalApplications_Events;
+use DoSomething\MBC_ExternalApplications\MBC_ExternalApplications_Events_Consumer;
 
-// Load configuration settings specific to this application
 require_once __DIR__ . '/mbc-externalApplications-events.config.inc';
 
-
+// Kick off
 echo '------- mbc-externalApplications-events START: ' . date('j D M Y G:i:s T') . ' -------', PHP_EOL;
 
-// Kick off
-$mb = new MessageBroker($credentials, $config);
-$mb->consumeMessage(array(new MBC_ExternalApplications_Events($credentials, $settings), 'consumeQueue'));
+$mb = $mbConfig->getProperty('messageBroker');
+$mb->consumeMessage(array(new MBC_ExternalApplications_Events_Consumer(), 'externalApplicationEventQueue'), QOS_SIZE);
 
 echo '------- mbc-externalApplications-events END: ' . date('j D M Y G:i:s T') . ' -------', PHP_EOL;
